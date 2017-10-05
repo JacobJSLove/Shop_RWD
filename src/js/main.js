7b94cd553126dfc25203394af1d85b22955c8572
JacobJSLove <kang2@o2.pl>
2017-10-05 13:04:42 +0200

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+function init() {
+  const imgDefer = document.getElementsByTagName( 'img' );
+  for ( let i = 0; i < imgDefer.length; i++ ) {
+    if ( imgDefer[ i ].getAttribute( 'data-src' ) ) {
+      imgDefer[ i ].setAttribute( 'src', imgDefer[ i ].getAttribute( 'data-src' ) );
+    }
+  }
+}
+window.onload = init;
+$( () => {
+  const cart = {
+    amount: 0,
+    value: 0
+  };
+  $( '.gallery .orange' ).click( function(){
+    const price = ( $( this ).closest( 'li' ).find( 'h5' ).text() );
+    const priceNumber = price.match( /\d+\W+\d+/ );
+    cart.value += parseFloat( priceNumber );
+    cart.amount ++;
+    $( '#cart' ).text( `${cart.value.toFixed( 2 )  } zł/prod. (${ cart.amount  })` );
+  } );
+
+} );
